132a3a382bd107b5b2d5d6aed62e40cf0698a4ac
Dejan Diklic <Dejan_Diklic00@live.com>
2022-12-13 11:56:36 +0100

    feat(Setup):ENV config

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 import socketIO from 'socket.io-client';
+import {useEffect} from "react";
+
 const socket = socketIO.connect('http://localhost:4000');
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    useEffect(() => {
+        socket.on("response", (data) => {
+            console.log(data)
+            alert(data)
+        })
+    }, [socket])
+
+    const handleClick = () => {
+        socket.emit("test", "test");
+    }
+
+    console.log(process.env)
+
+    return (
+        <div>
+            <button onClick={handleClick}>TEST</button>
+        </div>
+    );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config({path: "../.env"});
 const app = express();
 const PORT = 4000;
 
@@ -21,6 +22,13 @@ socketIO.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('🔥: A user disconnected');
     });
+
+    socket.on('test', (data) => {
+        console.log(process.env.REACT_APP_MODE)
+        console.log(data);
+        socket.emit("response", data + "-back")
+    });
+
 });
 
 app.get('/api', (req, res) => {
